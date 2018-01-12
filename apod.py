576f24927e2ca215fdb68583ef9ec0ed52272370
codl <codl@codl.fr>
2018-01-12 22:35:42 +0100

    force ipv4 because ipv6 is broken on apod.nasa.gov

--- apod.py
+++ apod.py
@@ -6,6 +6,10 @@ import re
 from urllib.parse import urljoin, urlparse
 import mimetypes
 
+import socket
+import requests.packages.urllib3.util.connection as urllib3_cn
+
+urllib3_cn.allowed_gai_family = lambda: socket.AF_INET
 
 class ApodBot(ananas.PineappleBot):
 
