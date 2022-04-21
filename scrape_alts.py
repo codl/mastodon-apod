c43917fc255e0ccde96d3f423bbec2b03b651ee7
codl <codl@codl.fr>
2022-04-22 00:32:56 +0200

    alt checker tool: be kinder, don't stop at 100, log weird alts

--- scrape_alts.py
+++ scrape_alts.py
@@ -5,19 +5,39 @@ import sys
 import csv
 from urllib.parse import urljoin
 import re
+import time
 
 if __name__ == '__main__':
     s = requests.session()
+    s.headers.update({
+        'user-agent': 'mastodon-apod +https://github.com/codl/mastodon-apod'})
     ARCHIVE = 'https://apod.nasa.gov/apod/archivepix.html'
     resp = s.get(ARCHIVE)
     soup = BeautifulSoup(resp.text, 'html.parser')
 
-    csvw = csv.writer(sys.stdout, dialect="unix")
+    with open('alts.csv', 'w') as altsf:
+        with open('alts_abnormal.csv', 'w') as altsabf:
+            csvw = csv.writer(altsf, dialect="unix")
+            ab_csvw = csv.writer(altsabf, dialect="unix")
 
-    for a, _ in zip(soup.find_all("a", href=re.compile("ap[0-9]{6}.html")), range(100)):
-        url = urljoin(ARCHIVE, a['href'])
+            delay = .1
 
-        resp = s.get(url)
-        page = ApodPage.from_html(url, resp.text)
+            for a in soup.find_all("a", href=re.compile("ap[0-9]{6}.html")):
+                url = urljoin(ARCHIVE, a['href'])
+                print(url)
 
-        csvw.writerow((page.url, page.alt))
+                resp = None
+                while not resp or resp.status_code != 200:
+                    time.sleep(delay)
+                    resp = s.get(url)
+                    if resp.status_code != 200:
+                        resp = None
+                        delay *= 2
+                        print("Raising delay to {}".format(url, delay))
+                page = ApodPage.from_html(url, resp.text)
+
+                csvw.writerow((page.url, page.alt))
+
+                if page.alt and not page.alt.startswith("See Explanation.") and not page.alt.endswith("Please see the explanation for more detailed information."):
+                    ab_csvw.writerow((page.url, page.alt))
+
