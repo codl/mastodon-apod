b857c8040e9061ad40905ac0c52525569f85d768
codl <codl@codl.fr>
2023-09-25 14:07:59 +0200

    xfail test for #179

--- tests/test_apod_bot.py
+++ tests/test_apod_bot.py
@@ -15,6 +15,21 @@ def test_fit_media_scales_image(testdata):
     i = Image.open(out.io)
     assert i.width * i.height <= 1080 * 1080
 
+@pytest.mark.xfail # https://github.com/codl/mastodon-apod/issues/179
+def test_fit_media_respects_exif_orientation(testdata):
+    upside_down = testdata("upside_down.jpg")
+    out = ApodBot.fit_media(upside_down)
+    i = Image.open(out.io)
+    top_pixel = i.getpixel((0, 0))
+    bot_pixel = i.getpixel((0, i.height-1))
+    if isinstance(top_pixel, tuple):
+        top_pixel = sum(top_pixel)
+    if isinstance(bot_pixel, tuple):
+        bot_pixel = sum(bot_pixel)
+    assert top_pixel > bot_pixel
+
+    
+
 @pytest.mark.xfail  # would fix https://github.com/codl/mastodon-apod/issues/122
 def test_fit_media_converts_gif_to_video(testdata):
     animated_gif = testdata("animated.gif")
